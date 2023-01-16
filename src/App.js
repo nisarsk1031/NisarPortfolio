95c21bbdca0137c74b920fa23fa2efdb5ebf907f
Nisarsk <nisarsk1031@gmail.com>
2023-01-16 23:21:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import{Routes,Route} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Project" element={<Project/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Contact" element={<Contact/>} />
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heroimg.jsx
@@ -0,0 +1,26 @@
+import "./Heroimgstyle.css";
+import React from 'react';
+import IntroImg from "../assets/intro-bg.jpg"
+import { Link } from "react-router-dom";
+
+const Heroimg = () => {
+  return (
+    <div className="hero">
+    <div className="mask">
+        <img className="introimg"
+        src={IntroImg} alt="IntroImg"/>
+    </div>
+    <div className="content">
+        <p> Hi, I'm Nisar</p>
+        <h1>React Developer</h1>
+        <div>
+            <Link to="/Project" className="btn">Projects</Link>
+            <Link to="/Contact" className="btn btn-light">Contact</Link>
+        </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Heroimg
